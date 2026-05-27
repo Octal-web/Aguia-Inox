a6393ed5b28a5c28fca06b75149e361fb4db0a51
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-05-27 08:45:14 -0300

    feat: adiciona conteudo na Home do manager

--- resources/js/Pages/Manager/Home/index.jsx
+++ resources/js/Pages/Manager/Home/index.jsx
@@ -56,6 +56,9 @@ const Page = () => {
             <FormContent content={conteudos[1]} full={true} idioma={idioma.codigo} />
             
             <FormContent content={conteudos[2]} full={true} idioma={idioma.codigo} />
+            
+            <FormContent content={conteudos[3]} full={true} idioma={idioma.codigo} />
+
         </AdminLayout>
     );
 };
