fb76bc4a4fd06f28aa744e78e9d21f033bebd625
RafaLaste <rafinhalaste@hotmail.com>
2026-07-27 14:48:01 -0300

    ci: imagens dos opcionais visíveis

--- resources/js/Components/OptionalItem.jsx
+++ resources/js/Components/OptionalItem.jsx
@@ -26,7 +26,7 @@ export const OptionalItem = ({ item }) => {
                             <img
                                 src={modelo.imagem}
                                 alt={modelo.nome}
-                                className="max-w-sm shrink-0 hidden"
+                                className="max-w-sm shrink-0 mb-4"
                             />
                             <div>
                                 <h3 className="text-2xl font-semibold text-primary">
